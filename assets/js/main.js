b5c3bcd6551b5b9d9b17719252822ce1ca1b6aa7
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 12:16:40 +0100

    start

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+console.log("klappts?");
+
+/* 
+Frage
+
+2x Variablen    fristName lastName
+
+zeige länge in der console
+
+Variable fullName soll Summe der beiden oben sein
+
+länge von fullName 
+*/
